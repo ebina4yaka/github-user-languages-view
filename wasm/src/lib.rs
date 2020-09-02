8ff63e262207e6411493612344153c9f1bc0aff8
ebina4yaka <4yaka3bina@gmail.com>
2020-09-02 16:39:27 +0900

    view github avatar

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,19 +1,41 @@
-mod utils;
-
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GithubUser {
+    pub avatar_url: String,
+    pub name: String,
+    pub html_url: String,
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, Next.js and Wasm!");
+#[wasm_bindgen(js_name = getGithubUser)]
+pub async fn get_github_user(user_name: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/users/{}", user_name);
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    (json.into_serde() as Result<GithubUser, serde_json::error::Error>)
+        .map(|user_info| JsValue::from_serde(&user_info).unwrap())
+        .map_err(|e| JsValue::from_str(&format!("{}", e)))
 }
